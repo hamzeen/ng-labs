72811aa1e543fb56b45a9416c25272bc219e5708
Hamzeen. H <hamzeen@gmail.com>
2019-04-10 16:05:38 +0200

    Update user.component.ts

--- src/app/users/user.component.ts
+++ src/app/users/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from '../shared/models/user';
+import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -19,5 +21,22 @@ export class UserComponent {
   selectUser(user) {
     this.activeUser = user;
     console.log(this.activeUser);
+  }
+  
+  public download1() {
+    console.log('PDF on the way...');
+    let doc = new jsPDF();
+    doc.text(20, 20, 'Hello world!');
+    doc.addPage();
+    doc.text(20, 20, 'Hi again this I page two');
+    doc.save('Test.pdf');
+  }
+    download(){
+    html2canvas(document.getElementById('results')).then(function(canvas) {
+      var img = canvas.toDataURL("image/png");
+      var doc = new jsPDF();
+      doc.addImage(img,'JPEG',5,20);
+      doc.save('testCanvas.pdf');
+    });
   }
 }
