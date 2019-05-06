61f4a02e2aadc62f87fbbe8869530844654ba085
Hamzeen. H <hamzeen@gmail.com>
2019-05-06 10:24:26 +0200

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -9,18 +10,19 @@ describe('AppComponent', () => {
         AppComponent
       ],
       imports:[RouterModule.forRoot([])],
+      providers: [{provide: APP_BASE_HREF, useValue : '/' }]
     }).compileComponents();
   }));
 
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    // expect(app).toBeTruthy();
+    expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    // expect(app.title).toEqual('2017 Driver Standings');
+    expect(app.title).toEqual('2017 Driver Standings');
   }));
 });
