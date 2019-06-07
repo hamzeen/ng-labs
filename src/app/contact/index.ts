cb90793415d2bf3f049cb79bfcedff7247e2879e
Hamzeen <hamzeen@gmail.com>
2019-06-07 22:46:07 +0200

    gh-pages commit

--- src/app/contact/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './contact.component';
-export * from './awesome.pipe';
--- src/app/shared/models/crisis.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Definition of Crisis Class
- */
-export class Crisis {
-  id: number;
-  location: string;
-}
